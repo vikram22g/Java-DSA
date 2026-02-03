2cde1bf9ce66e56459d921caab3de5bc3539b7e5
202122g <vikramjeetji33463@gmail.com>
2026-02-03 20:03:58 +0530

    Recrusion

---
+++ 17 Recursion/recursion.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class recursion{
+    public static void printNumber(int n){
+        if(n==6){
+            return ;
+        }
+        System.out.println(n);
+        printNumber(n+1);
+    }
+    public static void main(String args[]){
+        int n=1;
+        printNumber(n);
+    }
+}
